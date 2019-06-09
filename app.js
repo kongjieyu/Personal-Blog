f4b79f49c0875569df073dcc78aa2a9912a2215a
May Kong <1285235080@qq.com>
2019-06-09 15:40:44 +1200

    Post using postman

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 const http = require('http')
-const querystring = require('querystring')
 
+//GET
+// const querystring = require('querystring')
+// const server = http.createServer((req,res)=>{
+//     console.log('method', req.method) //GET
+//     const url = req.url
+//     console.log('url: ', url)
+//     req.query = querystring.parse(url.split('?')[1])
+//     console.log('query: ', req.query)
+//     res.end(
+//         JSON.stringify(req.query)
+//     )
+// })
+
+//POST
 const server = http.createServer((req,res)=>{
-    console.log('method', req.method) //GET
-    const url = req.url
-    console.log('url: ', url)
-    req.query = querystring.parse(url.split('?')[1])
-    console.log('query: ', req.query)
-    res.end(
-        JSON.stringify(req.query)
-    )
+    if (req.method === 'POST'){
+        //req 数据格式
+        console.log('req content type:', req.headers['content-type'])
+        //接收数据
+        let postData = ''
+        req.on('data', chunk => {
+            postData += chunk.toString()
+        })
+        req.on('end', () => {
+            console.log('postData:', postData)
+            res.end('Hello world')
+        })
+    }
 })
 
 server.listen(8000) 
