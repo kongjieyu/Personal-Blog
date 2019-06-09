68efb7732d249285a29577767d255d439ad31827
May Kong <1285235080@qq.com>
2019-06-09 17:08:16 +1200

    get and post;2C

--- app.js
+++ app.js
@@ -14,20 +14,59 @@ const http = require('http')
 // })
 
 //POST
+// const server = http.createServer((req,res)=>{
+//     if (req.method === 'POST'){
+//         //req 数据格式
+//         console.log('req content type:', req.headers['content-type'])
+//         //接收数据
+//         let postData = ''
+//         req.on('data', chunk => {
+//             postData += chunk.toString()
+//         })
+//         req.on('end', () => {
+//             console.log('postData:', postData)
+//             res.end('Hello world')
+//         })
+//     }
+// })
+const querystring = require('querystring')
 const server = http.createServer((req,res)=>{
-    if (req.method === 'POST'){
-        //req 数据格式
+    const method = req.method
+    console.log('method', req.method) //GET
+    const url = req.url
+    const path = url.split('?')[0]
+    const query = querystring.parse(url.split('?')[1])
+
+    //设置返回的格式为JSON格式
+    res.setHeader('Content-type', 'application/json')
+
+    //返回的数据
+    const resData = {
+        method,
+        url,
+        path,
+        query
+    }
+
+    if(method==='GET'){
+        res.end(
+            JSON.stringify(resData)
+        )
+    }
+    if(method==='POST'){
         console.log('req content type:', req.headers['content-type'])
-        //接收数据
         let postData = ''
         req.on('data', chunk => {
             postData += chunk.toString()
         })
         req.on('end', () => {
-            console.log('postData:', postData)
-            res.end('Hello world')
+            resData.postData = postData
+            res.end(
+                 JSON.stringify(resData)
+            )
         })
     }
+
 })
 
 server.listen(8000) 
